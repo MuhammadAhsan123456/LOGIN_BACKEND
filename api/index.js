9b6b2b8e91a616d6813f3fb336dd3d5b57de16ce
MuhammadAhsan123456 <ahsan663399@gmail.com>
2025-07-31 14:58:04 +0500

    chore: added README in src to make folder visible

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const { createServer } = require('http');
-const app = require('../src/app'); // yeh tumhara Express app hai
-
-module.exports = (req, res) => {
-  const server = createServer(app);
-  return server.emit('request', req, res);
-};
--- src/app.js
+++ src/app.js
@@ -1,68 +1,38 @@
 
-// const express = require("express")
-// const cors = require("cors"); // ✅ CORS import
-// const { connectToDB } = require("./config/database")
-// const { appRouter } = require("./router/auth"); 
-// const { userRouter } = require("./router/user");
-// const cookieParser = require("cookie-parser");
-
-
-// const app = express()
-// app.use(express.json())
-// app.use(cookieParser());
-
-
-// // server.js or app.js — jahan tumne express & cors use kiya hai
-
-// app.use(cors({
-//   origin: "http://localhost:5173", // ✅ only one https://
-//   credentials: true
-// }));
-
-
-
-// app.use(appRouter); 
-// app.use("/api", userRouter);
-
-// connectToDB().then(() => {
-//     console.log("Database is connected to MongoDB")
-// }).catch((err) => {
-//     console.log(err)
-// })
-
-// app.listen(5000, () => {
-//     console.log("Server is running on port 5000")
-// })
-
-const express = require("express");
-const cors = require("cors");
-const { connectToDB } = require("./config/database");
-const { appRouter } = require("./router/auth");
+const express = require("express")
+const cors = require("cors"); // ✅ CORS import
+const { connectToDB } = require("./config/database")
+const { appRouter } = require("./router/auth"); 
 const { userRouter } = require("./router/user");
 const cookieParser = require("cookie-parser");
 
-const app = express();
-app.use(express.json());
+
+const app = express()
+app.use(express.json())
 app.use(cookieParser());
 
+
+// server.js or app.js — jahan tumne express & cors use kiya hai
+
 app.use(cors({
-  origin: "https://iridescent-peony-bd857f.netlify.app",
+  origin: "http://localhost:5173", // ✅ only one https://
   credentials: true
 }));
 
-app.use(appRouter);
+
+
+app.use(appRouter); 
 app.use("/api", userRouter);
 
-// ✅ Add this route at the end
-app.get("/", (req, res) => {
-  res.send("Backend is working!");
-});
+connectToDB().then(() => {
+    console.log("Database is connected to MongoDB")
+}).catch((err) => {
+    console.log(err)
+})
 
-connectToDB()
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error("MongoDB Error:", err));
-
-module.exports = app; // ✅ sirf export, no app.listen()
+app.listen(5000, () => {
+    console.log("Server is running on port 5000")
+})
 
 
 
